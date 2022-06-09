ba84270ef96b97f2109f3fda67a25615a8b92742
Gavenda <gavenda@disroot.org>
2022-06-10 05:47:40 +0800

    Fix chat listener

--- yawa/src/main/kotlin/work/gavenda/yawa/chat/BukkitChatListener.kt
+++ yawa/src/main/kotlin/work/gavenda/yawa/chat/BukkitChatListener.kt
@@ -1,29 +1,23 @@
 package work.gavenda.yawa.chat
 
-import net.kyori.adventure.text.Component
 import org.bukkit.event.EventHandler
 import org.bukkit.event.EventPriority
 import org.bukkit.event.Listener
 import org.bukkit.event.player.AsyncPlayerChatEvent
 import work.gavenda.yawa.Config
-import work.gavenda.yawa.api.compat.sendMessageCompat
 import work.gavenda.yawa.api.placeholder.Placeholders
+import work.gavenda.yawa.api.toLegacyText
 
 class BukkitChatListener : Listener {
 
     @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     @Suppress("DEPRECATION")
     fun onPlayerChat(e: AsyncPlayerChatEvent) {
-        e.isCancelled = true
-
-        val chatMessage = Placeholders
+        val chatFormatComponent = Placeholders
             .withContext(e.player)
             .parse(Config.Chat.FormatMessage)
-            .append(Component.text(e.message))
-
-        e.recipients.forEach {
-            it.sendMessageCompat(chatMessage)
-        }
+        val chatFormat = chatFormatComponent.toLegacyText()
+        e.format = chatFormat.plus("%2\$s")
     }
 
 }
